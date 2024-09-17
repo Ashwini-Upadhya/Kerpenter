2d608de386150a255d3fae667136649ce3c8e081
Ashwini Upadhyay <ashwaniupadhyay9015@gmail.com>
2024-09-17 17:46:18 +0530

    division of css

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,9 +4,9 @@ import './Header.css'
 export default function Header () {
     return(
     <div className="header">
-        <img className='logo' src='Group 2 1.svg'></img>
-        <img className='call' src='Call.svg'></img>
-        <img className='mail' src='Mail.svg'></img>
+        <img className='logo' src='public/Group 2 1.svg'></img>
+        <img className='call' src='public/Call.svg'></img>
+        <img className='mail' src='public/Mail.svg'></img>
     </div>
        
     )
